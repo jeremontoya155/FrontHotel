9c4c491c6865c46c99b0e7f95445b2a28ec8f2bf
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-07-10 19:40:23 -0300

    Unir

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
-=======
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
->>>>>>> 09e75ab43f5ffa1fdd0dcae933d5cc38652e3921
+
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
