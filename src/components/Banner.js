298256b5de2935096766e5479f85043f58aed923
ManiaciNahuel <nahuelmaniaci@gmail.com>
2024-07-10 14:07:55 -0300

    prueba

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Banner = () => {
   return (
     <div className="banner">
-      <h1>Bienvenido a la Lista de Hoteles</h1>
+      <h1>Bienvenido a la Lista de Hoteles 123</h1>
       {/* Puedes agregar contenido adicional aquí si es necesario */}
     </div>
   );
