a89a62cb30ebe7185f79cccffdc9bd834f16873e
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-07-10 01:02:27 -0300

    Search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaSlidersH } from 'react-icons/fa';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Elegí tu próximo destino..."
+        className="search-bar__input search-bar__input-dest"
+      />
+      <div className="search-bar__dates">
+        <input
+          type="date"
+          placeholder="Fecha de Llegada"
+          className="search-bar__date"
+        /> 
+        <input
+          type="date"
+          placeholder="Fecha de Salida"
+          className="search-bar__date"
+        />
+      </div>
+      <input
+        type="number"
+        placeholder="Huéspedes"
+        className="search-bar__input search-bar__input-huesp"
+      />
+      <button className="search-bar__button">Buscar</button>
+      <button className="search-bar__settings">
+        <FaSlidersH />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
