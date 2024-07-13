4ac3e65ab950c12ffcff7bb4f85658e06ad68aff
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-07-13 19:25:48 -0300

    Link 3.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
 
       <div className="navigation-wrapper">
         <div className="navigation">
-          <Link className="nav-link" href='/'>INICIO</Link>
-          <Link className="nav-link" href='/hotels'>HOSPEDAJES</Link>
-          <Link className="nav-link" href='/'>NOSOTROS</Link>
+          <Link className="nav-link" to='/'>INICIO</Link>
+          <Link className="nav-link" to='/hotels'>HOSPEDAJES</Link>
+          <Link className="nav-link" to='/'>NOSOTROS</Link>
           <a
             className="nav-link"
             href="https://wa.me/5493517045448?text=Hola!%20Estaba%20buscando%20un%20hospedaje%20para..."
